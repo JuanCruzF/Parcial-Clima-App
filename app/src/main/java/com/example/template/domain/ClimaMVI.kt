641f2581c44d008a839a0fb2405404e14fad0fbc
JuanCruzF <juancruzfucile@hotmail.com>
2025-11-11 23:07:54 -0300

    Closes #3: agrega módulo Clima (vista + ViewModel)

---
+++ app/src/main/java/com/example/template/domain/ClimaMVI.kt
@@ -0,0 +1,30 @@
+package com.example.template.domain
+
+
+//patron mvi (state e intent para clima)
+
+
+// --- STATE --
+
+//todo el estado de pantalla de clima
+
+data class ClimaState(
+    val ciudad: String = "Cargando...",
+    val temperatura: Double = 0.0,
+    val descripcion: String = "",
+    val isLoading: Boolean = true,
+    //manejar error
+
+    val error: String? = null
+)
+
+
+// --- INTENT ---
+//acciones de usuario o sistema - interfaz sellada para limitar acciones
+sealed interface ClimaIntent {
+    // carga por primera vez
+    object CargarClimaInicial : ClimaIntent
+
+    // al actualizar (más adelante) TODO
+    // object RefrescarClima : ClimaIntent
+}
