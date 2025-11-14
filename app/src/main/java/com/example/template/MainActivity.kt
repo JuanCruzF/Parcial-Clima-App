d99b0f7d0c0e32e49d83e1be09f7ba35cabb6594
JuanCruzF <juancruzfucile@hotmail.com>
2025-11-14 10:02:27 -0300

    Issue #4: Router & Navegación

--- app/src/main/java/com/example/template/MainActivity.kt
+++ app/src/main/java/com/example/template/MainActivity.kt
@@ -3,23 +3,60 @@ package com.example.template
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.template.ui.theme.TemplateTheme
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.template.ui.theme.ClimaAppTheme
+import com.example.template.view.CiudadesView
+import com.example.template.view.ClimaView
+import com.example.template.viewmodel.TemaViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // tema
             val temaViewModel: TemaViewModel = viewModel()
-            ClimaAppTheme(darkTheme = temaViewModel.isDarkMode.collectAsState().value) {
-                CiudadesView(temaViewModel = temaViewModel)
+            val isDark by temaViewModel.isDarkMode.collectAsState()
+
+            ClimaAppTheme(darkTheme = isDark) {
+
+                // flujo condicional
+
+                // simulacion pero va a venir de SharedPreferences o DataStore)
+                val hayCiudadGuardada = false
+
+                // loogica pantalla de arranque
+                val startDestination = if (hayCiudadGuardada) {
+                    "clima" // si hay ciudad, vamos al clima
+                } else {
+                    "ciudades" // Si no hay, vamos a la lista
+                }
+
+
+                // controlador para manear pantallas
+                val navController = rememberNavController()
+
+                // contenedor donde se van a mostrar las pantallas
+                NavHost(navController = navController, startDestination = startDestination) {
+
+                    // lista/buscador de ciudades
+                    composable(route = "ciudades") {
+                        CiudadesView(
+                            onCiudadSeleccionada = {
+                                navController.navigate("clima")
+                            }
+                        )
+                    }
+
+                    //detalles del clima
+                    composable(route = "clima") {
+                        ClimaView(temaViewModel = temaViewModel)
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/template/view/ClimaView.kt
@@ -0,0 +1,137 @@
+package com.example.template.view
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.template.domain.ClimaIntent
+import com.example.template.domain.ClimaState
+import com.example.template.viewmodel.ClimaViewModel
+import com.example.template.viewmodel.TemaViewModel
+
+
+//composable raíz de la app - función de "View" en MVI.
+
+@Composable
+fun ClimaView(temaViewModel: TemaViewModel) {
+
+    // creamos la instancia de
+    val climaViewModel: ClimaViewModel = viewModel()
+
+    // vemos el state del ViewModel.
+    // state se actualiza automáticamente cada vez que el VM emita uno nuevo.
+    val state by climaViewModel.state.collectAsState()
+
+    //intent inicial
+    LaunchedEffect(Unit) {
+        climaViewModel.handleIntent(ClimaIntent.CargarClimaInicial)
+    }
+
+    // llamado a UI
+    ClimaScreen(
+        state = state,
+        onIntent = { intent ->
+            climaViewModel.handleIntent(intent)
+        }
+    )
+}
+
+//layout
+
+@Composable
+fun ClimaScreen(state: ClimaState, onIntent: (ClimaIntent) -> Unit) {
+
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+
+        // cargando...
+        if (state.isLoading) {
+            //spinner
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator(modifier = Modifier.size(64.dp))
+            }
+
+        //en caso de error
+        } else if (state.error != null) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = state.error,
+                    color = Color.Red,
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+
+            // si no está cargando y no hay error
+        } else {
+            //datos del clima
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+
+                //día Actual
+                Text(
+                    text = state.ciudad,
+                    fontSize = 32.sp,
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Text(
+                    text = "${state.temperatura}°C",
+                    fontSize = 56.sp
+                )
+                Text(
+                    text = state.descripcion,
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                //placeholder
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .border(1.dp, Color.Gray)
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Acá iría el pronostico",
+                        textAlign = TextAlign.Center,
+                        color = Color.Gray
+                    )
+                }
+            }
+        }
+    }
+}
